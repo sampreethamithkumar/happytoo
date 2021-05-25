a03820a8e8b2105aa8c945607d9d415b5f634722
sampreethamithkumar <sampreethamith65@gmail.com>
2021-05-26 00:14:06 +1000

    api uri changed to live server

--- app/services/httpService.js
+++ app/services/httpService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:5000/api";
+axios.defaults.baseURL = "https://tranquil-escarpment-11384.herokuapp.com/api";
 
 export default {
   post: axios.post,
